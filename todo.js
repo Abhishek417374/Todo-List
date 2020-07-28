a1a1f57bbe721a8f9de574ad6115aa67a20c9d1d
Abhishek <abhishek417374@gmail.com>
2020-07-28 19:35:15 +0530

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,34 @@
+const todoinput=document.querySelector('todo-input');
+const todobutton=document.querySelector('todo-btn');
+const todolist=document.querySelector('todo-list');
+todobutton.addEventListener('click' , addTodo);
+
+function addTodo(){
+
+
+    const tododiv=document.createElement("div");
+    tododiv.classList.add('todo');
+
+    const newtodo=document.createElement('li');
+    newtodo.classList.add('todo-item');
+    newtodo.innerText='abhishek';
+    tododiv.appendChild(newtodo);
+         
+    const checkbutton=document.createElement('button');
+    checkbutton.classList.add('check-button');
+    tododiv.appendChild(checkbutton);
+    checkbutton.innerHTML='<i class="fas fa-check"></i>';
+
+    const deletebutton=document.createElement('button');
+    deletebutton.classList.add('delete-button');
+    tododiv.appendChild(deletebutton);
+    deletebutton.innerHTML='<i class="fas fa-trash"></i>';
+
+    todolist.appendChild(tododiv);
+
+
+} 
+function myfunction(){
+    document.getElementById("checking").innerHTML="abhishek kumar tiwari";
+}
+
