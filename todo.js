750a85eb792bc7c74fa1ef275ec1987ce15535d8
Abhishek <abhishek417374@gmail.com>
2020-07-30 21:34:25 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,34 +1,30 @@
-const todoinput=document.querySelector('todo-input');
-const todobutton=document.querySelector('todo-btn');
-const todolist=document.querySelector('todo-list');
-todobutton.addEventListener('click' , addTodo);
+const todoinput=document.querySelector('.todo-input');
+const todobutton=document.querySelector('.todo-btn');
+const todolist=document.querySelector('.todo-list');
+todobutton.addEventListener("click" , checking);
 
-function addTodo(){
-
-
+function checking(){
     const tododiv=document.createElement("div");
     tododiv.classList.add('todo');
 
     const newtodo=document.createElement('li');
     newtodo.classList.add('todo-item');
-    newtodo.innerText='abhishek';
+   newtodo.innerText='abhishek';
     tododiv.appendChild(newtodo);
          
     const checkbutton=document.createElement('button');
     checkbutton.classList.add('check-button');
-    tododiv.appendChild(checkbutton);
-    checkbutton.innerHTML='<i class="fas fa-check"></i>';
+   tododiv.appendChild(checkbutton);
+   checkbutton.innerHTML='<i class="fas fa-check"></i>';
 
-    const deletebutton=document.createElement('button');
-    deletebutton.classList.add('delete-button');
-    tododiv.appendChild(deletebutton);
-    deletebutton.innerHTML='<i class="fas fa-trash"></i>';
+   const deletebutton=document.createElement('button');
+   deletebutton.classList.add('delete-button');
+   tododiv.appendChild(deletebutton);
+   deletebutton.innerHTML='<i class="fas fa-trash"></i>';
 
     todolist.appendChild(tododiv);
-
-
-} 
-function myfunction(){
-    document.getElementById("checking").innerHTML="abhishek kumar tiwari";
 }
+
+
+
 
